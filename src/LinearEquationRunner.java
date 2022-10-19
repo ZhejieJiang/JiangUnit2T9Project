06cfd730da579a5087fe6034c5190992dc257b01
Zhejie Jiang <zhejiej@nycstudents.net>
2022-10-19 09:29:03 -0400

    working

--- src/LinearEquationRunner.java
+++ src/LinearEquationRunner.java
@@ -3,29 +3,29 @@ public class LinearEquationRunner {
     public static void main(String[]args){
         Scanner myScanner = new Scanner(System.in);
         System.out.println("Welcome!");
-        System.out.println("Enter coordinate 1: ");
+        System.out.print("Enter coordinate 1: ");
         String coordinate1 = myScanner.nextLine();
-        int x1 = Integer.parseInt(coordinate1.substring(coordinate1.indexOf("("),coordinate1.indexOf(",")));
-        System.out.println("Enter coordinate 2: ");
+
+        int x1 = Integer.parseInt(coordinate1.substring(coordinate1.indexOf("(") +1,coordinate1.indexOf(",")));
+        int y1 = Integer.parseInt(coordinate1.substring(coordinate1.indexOf(" ") + 1,coordinate1.indexOf(")")));
+        System.out.print("Enter coordinate 2: ");
         String coordinate2 = myScanner.nextLine();
 
-
+        int x2 = Integer.parseInt(coordinate2.substring(coordinate2.indexOf("(") +1,coordinate2.indexOf(",")));
+        int y2 = Integer.parseInt(coordinate2.substring(coordinate2.indexOf(" ") +1,coordinate2.indexOf(")")));
 
         LinearEquation equation = new LinearEquation(x1, y1, x2, y2);
-        System.out.println("Equation: " + equation.equation());
-        System.out.println("Slope: " + equation.slope());
-        System.out.println("y-intercept: " + equation.yIntercept());
-        System.out.println("Distance: " + equation.distance());
+
         System.out.println();
         System.out.println("----- Line info -----");
         System.out.println(equation.lineInfo());
         System.out.println();
-        double testX = 4;
+
+        System.out.print("Enter a value for x: ");
+        int testX = myScanner.nextInt();
         System.out.println("Coordinate for x: " + equation.coordinateForX(testX));
 
 
 
-        System.out.println("Enter a value for x: ");
-        int x3 = myScanner.nextInt();
     }
 }
